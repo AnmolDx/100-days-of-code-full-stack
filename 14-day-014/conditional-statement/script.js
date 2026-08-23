1cbea72e392084eaea68773f851a0b3633e05fce
Anmol <129307316+AnmolDx@users.noreply.github.com>
2026-08-23 18:19:55 +0530

    revise about else statement

--- 14-day-014/conditional-statement/script.js
+++ 14-day-014/conditional-statement/script.js
@@ -1,8 +1,29 @@
-let age = 23
-if (age >=18) {
-    console.log("You can vote")
-    console.log("You can also drive")
-}
-if (age < 18) {
-    console.log("You cannot vote")
+// let age = 23
+// if (age >=18) {
+//     console.log("You can vote")
+//     console.log("You can also drive")
+// }
+// if (age < 18) {
+//     console.log("You cannot vote")
+// }
+
+//else if statement
+// let age = 14;
+// if(age >= 18){
+//     console.log("You can vote")
+// }
+// else if(age < 18){
+//     console.log("You cannot vote")
+// }
+
+//else statement
+let color = "pink"
+if(color === "red"){
+    console.log("stop");
+}else if(color === "yellow"){
+    console.log("wait");
+}else if(color === "green"){
+    console.log("go")
+}else{
+    console.log("The traffic light is broken")
 }
--- 14-day-014/notes.md
+++ 14-day-014/notes.md
@@ -11,3 +11,7 @@ to write(log) message on the console.
 ## Conditional Statement
 
 - if statement
+
+- else if statement - if first `if` condition is false only then it will check and log the `else if` statement.
+
+- else statement - if `if` condition is false then it will log else statement without checking anything.

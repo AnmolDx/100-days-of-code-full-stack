c955e5f31294befe410718ab5d03f3ea527dcbb7
Anmol <129307316+AnmolDx@users.noreply.github.com>
2026-08-23 17:08:32 +0530

    revised about template literals

--- 14-day-014/linking-js-file/script.js
+++ 14-day-014/linking-js-file/script.js
@@ -2,3 +2,9 @@ console.log("hello world")
 let a = 5;
 let b = 8;
 console.log("sum is:",a + b)
+
+let pencilPrice = 5
+let penPrice = 10;
+
+console.log("the sum of price", (pencilPrice + penPrice), "rupees")
+console.log(`the sum of price ${pencilPrice + penPrice} rupees`)

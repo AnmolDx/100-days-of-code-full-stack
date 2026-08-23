c23ef5f2157a2f5fd3dc29b390a25031dcee1b78
Anmol <129307316+AnmolDx@users.noreply.github.com>
2026-08-23 18:27:09 +0530

    did 2nd practice question

---
+++ 14-day-014/practice2/script.js
@@ -0,0 +1,12 @@
+let size = "5"; //XL, L, M, S
+if(size === "XL"){
+    console.log("You have to pay 250rs")
+}else if(size === "L"){
+    console.log("You have to pay 200rs")
+}else if(size === "M"){
+    console.log("You have to pay 100rs")
+}else if(size === "S"){
+    console.log("You have to pay 50rs")
+}else{
+    console.log("We dont have this option, Sorry!!")
+}

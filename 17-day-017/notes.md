7681d688b620b25c0d0acde1e61d0f85527d6b75
Anmol <129307316+AnmolDx@users.noreply.github.com>
2026-08-26 18:24:56 +0530

    revise about object literals and did a practice question

---
+++ 17-day-017/notes.md
@@ -0,0 +1,7 @@
+# Notes
+
+## Objects Literals
+
+Used to store keyed collections & complex entities. Objects are a collection of properties.
+
+property => (key, value) pair
---
+++ 17-day-017/objects-literals/script.js
@@ -0,0 +1,16 @@
+let student = {
+    name: "Anmol",
+    age: 20,
+    marks: 99.9
+}
+
+//practice question
+
+// create object literal for properties of twitter post
+const post = {
+    username: "techwalaguy",
+    content: "today i learned about object literals",
+    like: 0,
+    reposts: 0,
+    tags: ["#100daysofcode", "#webdev", "#javaScript"]
+}

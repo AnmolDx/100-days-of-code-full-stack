3a2b38bb8994e875e1db42a68f68d1040e68f71b
Anmol <129307316+AnmolDx@users.noreply.github.com>
2026-08-27 22:02:16 +0530

    complete 2 practice questions

---
+++ 18-day-018/practice/script.js
@@ -0,0 +1,13 @@
+function poem() {
+    console.log("Johny Johny, Yes Papa, Eating Sugar? No Papa Are you sure Yes Papa Open your mouth Ha, ha, ha ")
+}
+
+poem()
+
+function roll(){
+    let dice = Math.floor(Math.random()*6)+1;
+    console.log(dice)
+}
+roll()
+roll()
+roll()

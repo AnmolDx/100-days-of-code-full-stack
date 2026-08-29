32c5687b947bc89f3cc720cc40873d425afa5d34
Anmol <129307316+AnmolDx@users.noreply.github.com>
2026-08-29 22:57:18 +0530

    learned about this with arrow func

--- 19-day-019/arrow-function/script.js
+++ 19-day-019/arrow-function/script.js
@@ -12,3 +12,32 @@ cube(5)
 
 const mul = (a,b) => (a*b)
 console.log(mul(3,5)) 
+
+//arrow func with this keyword
+
+const student = {
+    name: "anmol",
+    marks: 88,
+    prop: this,
+    getName: function(){
+        console.log(this)
+        return this.name;
+    },
+    getMarks: () => {
+        console.log(this) //parent's scope(window)
+        return this.marks;
+    },
+    getInfo1: function(){
+        setTimeout(() => {
+            console.log(this)
+        }, 2000);
+    },
+    getInfo2: function(){
+        setTimeout(function() {
+            console.log(this)
+        }, 2000);
+    },
+}
+
+student.getInfo1()
+student.getInfo2()

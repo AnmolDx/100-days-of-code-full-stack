6afb86d0c116197de2ddb6dce5f06955911456ac
Anmol <129307316+AnmolDx@users.noreply.github.com>
2026-09-01 22:12:57 +0530

    revised about addEventListener

---
+++ 22-day-022/event-listeners/script.js
@@ -0,0 +1,10 @@
+let btn = document.querySelector("button")
+btn.addEventListener("click", function() {
+    console.log("hello")
+})
+btn.addEventListener("click", function() {
+    console.log("Anmol")
+})
+btn.addEventListener("dblclick", function() {
+    console.log("you have double clicked the button")
+})
--- 22-day-022/notes.md
+++ 22-day-022/notes.md
@@ -7,3 +7,7 @@ Events are signals that something has occurred. (user inputs/actions)
 - onclick(when an element is click)
 
 - onmouseenter(when mouse enters an element)
+
+## Event Listener
+
+- `element.addEventListener(event, callback)`

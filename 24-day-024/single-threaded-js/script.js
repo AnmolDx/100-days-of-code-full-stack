bcc225c3ff314c3fb70548aef17fe3c24cb2f63f
Anmol <129307316+AnmolDx@users.noreply.github.com>
2026-09-03 23:31:17 +0530

    refactor color changing code with promises

--- 24-day-024/single-threaded-js/script.js
+++ 24-day-024/single-threaded-js/script.js
@@ -6,11 +6,13 @@
 
 let h1 = document.querySelector("h1")
 
-function changeColor(color, delay, nextColorChange){
-    setTimeout(() => {
-        h1.style.color = color
-        if(nextColorChange) nextColorChange()
-    }, delay);
+function changeColor(color, delay){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            h1.style.color = color;
+            resolve("color changed")
+        }, delay);
+    })
 }
 
 // setTimeout(() => {
@@ -25,10 +27,26 @@ function changeColor(color, delay, nextColorChange){
 //     h1.style.color = "blue"
 // }, 3000);
 
-changeColor("red",1000, () => {
-    changeColor("orange", 1000, () => {
-        changeColor("green", 1000, () => {
-            changeColor("yellow", 1000)
-        })
-    })
+// changeColor("red",1000, () => {
+//     changeColor("orange", 1000, () => {
+//         changeColor("green", 1000, () => {
+//             changeColor("yellow", 1000)
+//         })
+//     })
+// })
+
+changeColor("red", 5000)
+.then(() => {
+    console.log("red color was completed")
+    return changeColor("green", 1000)
+})
+.then(() => {
+    console.log("green color was completed")
+    return changeColor("blue", 1000)
+})
+.then(() => {
+    console.log("blue was completed")
+})
+.catch(() => {
+    console.log()
 })

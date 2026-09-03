4854727d8c4f40d8b1c0ff88af17f5c1177e5a4c
Anmol <129307316+AnmolDx@users.noreply.github.com>
2026-09-03 22:31:56 +0530

    learned JS nature and callback hell

--- 24-day-024/notes.md
+++ 24-day-024/notes.md
@@ -3,3 +3,7 @@
 ## Call Stack
 
 Follows LIFO(Last In First Out)
+
+## JS is Single Threaded
+
+Runs code one by one
---
+++ 24-day-024/single-threaded-js/script.js
@@ -0,0 +1,34 @@
+// setTimeout(() => {
+//     console.log("yoooooo")
+// }, 2000);
+
+// console.log("hello")
+
+let h1 = document.querySelector("h1")
+
+function changeColor(color, delay, nextColorChange){
+    setTimeout(() => {
+        h1.style.color = color
+        if(nextColorChange) nextColorChange()
+    }, delay);
+}
+
+// setTimeout(() => {
+//     h1.style.color = "red"
+// }, 1000);
+
+// setTimeout(() => {
+//     h1.style.color = "orange"
+// }, 2000);
+
+// setTimeout(() => {
+//     h1.style.color = "blue"
+// }, 3000);
+
+changeColor("red",1000, () => {
+    changeColor("orange", 1000, () => {
+        changeColor("green", 1000, () => {
+            changeColor("yellow", 1000)
+        })
+    })
+})

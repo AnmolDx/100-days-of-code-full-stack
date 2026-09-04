ed511e02b8417a78241c8944146d8a000dc3c751
Anmol <129307316+AnmolDx@users.noreply.github.com>
2026-09-04 20:10:43 +0530

    learned about async and await

---
+++ 25-day-025/async-function/script.js
@@ -0,0 +1,49 @@
+// async function greet(){
+//     throw("some error")
+//     return "hello"
+// }
+
+// greet()
+// .then((result) => {
+//     console.log("promise was resolved")
+//     console.log(result)
+
+// })
+// .catch((error) => {
+//     console.log("promise was rejected with error")
+//     console.log(error)
+// })
+
+function getNum(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let num = Math.floor(Math.random()*10)+1;
+            console.log(num)
+            resolve()
+        }, 1000);
+    })
+}
+// async function demo() {
+//     await getNum()
+//     await getNum()
+//     await getNum()
+//     getNum()
+// }
+
+let h1 = document.querySelector("h1")
+
+function changeColor(color, delay){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            h1.style.color = color;
+            resolve("color changed")
+        }, delay);
+    })
+}
+
+async function demo() {
+    await changeColor("red", 1000)
+    await changeColor("green", 1000)
+    await changeColor("yellow", 1000)
+    changeColor("blue", 1000)
+}
---
+++ 25-day-025/notes.md
@@ -0,0 +1,9 @@
+# Notes
+
+## Async Function
+
+returns promise after running the function
+
+### Await Keyword
+
+pauses the execution of its surrounding async function until the promise is settled (resolved or rejected)

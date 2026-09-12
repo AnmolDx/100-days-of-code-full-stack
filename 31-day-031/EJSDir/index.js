cbf8249063d247a81ac0562ee2dbaace1517da06
AnmolDx <129307316+AnmolDx@users.noreply.github.com>
2026-09-12 22:24:37 +0530

    learn about condition statement in ejs

--- 31-day-031/EJSDir/index.js
+++ 31-day-031/EJSDir/index.js
@@ -19,6 +19,10 @@ app.get("/rolldice", (req, res) => {
     let diceVal =  Math.floor(Math.random()*6)+1
     res.render("rolldice.ejs", {diceVal: diceVal})
 })
+app.get("/ig/:username", (req, res) => {
+    let {username} = req.params
+    res.render("instagram.ejs", {username})
+})
 
 app.listen(port, () => {
     console.log("Listening on port")

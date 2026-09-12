742f17ff737ead27ba3d2aa668870819d7e245e6
AnmolDx <129307316+AnmolDx@users.noreply.github.com>
2026-09-12 21:54:54 +0530

    start EJS and learn basics of it

---
+++ 31-day-031/EJSDir/index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+
+const port = 8080;
+
+app.set("view engine", "ejs")
+
+// app.get("/", (req, res) => {
+//     res.send("this is root")
+// } )
+
+app.set("views", path.join(__dirname, "/views"))
+
+app.get("/", (req, res) => {
+    res.render("home.ejs")
+})
+app.get("/rolldice", (req, res) => {
+    let diceVal =  Math.floor(Math.random()*6)+1
+    res.render("rolldice.ejs", {diceVal: diceVal})
+})
+
+app.listen(port, () => {
+    console.log("Listening on port")
+})
---
+++ 31-day-031/notes.md
@@ -0,0 +1,17 @@
+# Notes
+
+## Templating
+
+### EJS(Embedded JavaScript Templates)
+
+EJS is a simple templating language that lets you generate HTML markup with plain JavaScript.
+
+To send small html part or string we use `res.send("something")`
+
+To send file we use `res.render("file.ejs")`
+
+## Interpolation Syntax
+
+Interpolation refers to embedding expressions into marked up text.
+
+`<%= some js code %>` - this will help in embed js code in ejs the output will be in string.
